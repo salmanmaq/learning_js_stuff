7295690d6bb3ab9f8bc1c6e0c33ee669a8d0cbe5
Salman Maqbool <salmanmaq@gmail.com>
2021-09-19 15:34:53 +0500

    Corrected an error in http-server

--- 12-http.js
+++ 12-http.js
@@ -4,15 +4,17 @@ const server = http.createServer( (req, res) => {
     if (req.url === '/') {
         res.write('Welcome to our home page');
         res.end();
-    };
-    if (req.url === '/about') {
+    }
+    else if (req.url === '/about') {
         res.end('Here is our short histroy');
-    };
-    res.end(`
-        <h1>Oops!</h1>
-        <p>We can't seem to find the page you are looking for</p>
-        <a href="/">Back home</a>
-    `);
+    }
+    else {
+        res.end(`
+            <h1>Oops!</h1>
+            <p>We can't seem to find the page you are looking for</p>
+            <a href="/">Back home</a>
+        `);
+    }
 });
 
 server.listen(5000);
